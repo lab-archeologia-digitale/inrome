85b8c26edfc803d8d60992fab752683b49ad0d4d
Julian Bogdani <jbogdani@gmail.com>
2023-06-25 14:38:59 +0200

    Updates

---
+++ component---src-pages-404-js-ac9355aa3aa64fbe2b07.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinrome=self.webpackChunkinrome||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var a=n(7294),l=n(7707),u=n(1707);t.default=()=>a.createElement(l.Z,null,a.createElement("div",{className:"container text-center my-5"},a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")));const r=()=>a.createElement(u.p,{title:"404: Not found"})}}]);
+//# sourceMappingURL=component---src-pages-404-js-ac9355aa3aa64fbe2b07.js.map
