4995b051a78b36f1acef1099b716190d3125a514
Julian Bogdani <jbogdani@gmail.com>
2023-06-26 14:52:19 +0200

    removed docs

--- docs/component---src-pages-404-js-2b348e29a3c7724314cd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinrome=self.webpackChunkinrome||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var a=n(7294),l=n(2700),u=n(1707);t.default=()=>a.createElement(l.Z,null,a.createElement("div",{className:"container text-center my-5"},a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")));const r=()=>a.createElement(u.p,{title:"404: Not found"})}}]);
-//# sourceMappingURL=component---src-pages-404-js-2b348e29a3c7724314cd.js.map
