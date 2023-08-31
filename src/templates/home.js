f87cb528fd3feee758ba5f97a624329c9b13b0c7
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2023-08-31 20:39:30 +0200

    Update home.js

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,110 +1,98 @@
-import React from "react"
-import { Row, Col, Container, Card } from "react-bootstrap"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import Slide from "../components/slide"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
+import { Row, Col, Container, Card } from "react-bootstrap"
 
-export default function Article({ data, classes, pageContext }) {
-  const article = data.directus.cms_articles[0]
-  const lang = pageContext.lang
-  console.log(lang)
+//other
+import { StaticImage } from "gatsby-plugin-image"
 
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    {
+      directus {
+        cms_articles(limit: 1, filter: { slug: { _eq: "home" } }) {
+          id
+          title
+          slug
+          summary
+          text
+        }
+      }
+    }
+  `)
   return (
-    <Layout>
-      <div className="slide">
-        <Slide />
-      </div>
-      <div>
-        <section className="py-5">
-          <Container>
-            <Row>
-              <h1>{lang === "it" ? article.title_it : article.title}</h1>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: lang === "it" ? article.text_it : article.text,
-                }}
-              />
-            </Row>
-          </Container>
-        </section>
-        <Wrapper>
-          <section className="py-5 text-center news">
-            <Container>
-              <Row>
-                <Col sm={4} xs={12}>
-                  <Card>
-                    <StaticImage
-                      src="../images/progetti.png"
-                      formats={["AUTO", "WEBP"]}
-                      alt="progetti"
-                      className="card-img-top"
-                      height={300}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        {" "}
-                        <a href="/">Sub Projects</a>
-                      </h5>
-                    </div>
-                  </Card>
-                </Col>
-                <Col sm={4} xs={12}>
-                  <Card>
-                    <StaticImage
-                      src="../images/news.png"
-                      formats={["AUTO", "WEBP"]}
-                      alt="news"
-                      className="card-img-top"
-                      height={300}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        <a href="/">News</a>
-                      </h5>
-                    </div>
-                  </Card>
-                </Col>
-                <Col sm={4} xs={12}>
-                  <Card>
-                    <StaticImage
-                      src="../images/output.png"
-                      formats={["AUTO", "WEBP"]}
-                      alt="output"
-                      className="card-img-top"
-                      height={300}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        <a href="/">Outputs</a>
-                      </h5>
-                    </div>
-                  </Card>
-                </Col>
-              </Row>
-            </Container>
-          </section>
-        </Wrapper>
-      </div>
-    </Layout>
+    <Wrapper>
+      <section className="py-5">
+        <Container>
+          <Row>
+            <h1>{data.directus.cms_articles[0].title}</h1>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: data.directus.cms_articles[0].text,
+              }}
+            />
+          </Row>
+        </Container>
+      </section>
+      <section className="py-5 text-center news">
+        <Container>
+          <Row>
+            <Col sm={4} xs={12}>
+              <Card>
+                <StaticImage
+                  src="../images/progetti.png"
+                  formats={["AUTO", "WEBP"]}
+                  alt="progetti"
+                  className="card-img-top"
+                  height={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {" "}
+                    <a href="/">Sub Projects</a>
+                  </h5>
+                </div>
+              </Card>
+            </Col>
+            <Col sm={4} xs={12}>
+              <Card>
+                <StaticImage
+                  src="../images/news.png"
+                  formats={["AUTO", "WEBP"]}
+                  alt="news"
+                  className="card-img-top"
+                  height={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <a href="/">News</a>
+                  </h5>
+                </div>
+              </Card>
+            </Col>
+            <Col sm={4} xs={12}>
+              <Card>
+                <StaticImage
+                  src="../images/output.png"
+                  formats={["AUTO", "WEBP"]}
+                  alt="output"
+                  className="card-img-top"
+                  height={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <a href="/">Outputs</a>
+                  </h5>
+                </div>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Wrapper>
   )
 }
 
-export const query = graphql`
-  query ($slug: String!) {
-    directus {
-      cms_articles(filter: { slug: { _eq: $slug } }) {
-        title
-        title_it
-        summary
-        summary_it
-        text
-        text_it
-      }
-    }
-  }
-`
 //style
 const Wrapper = styled.section`
   .news {
@@ -133,3 +121,5 @@ const Wrapper = styled.section`
     }
   }
 `
+
+export default Home
