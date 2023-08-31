a2d51a35d531ed288f67162f976bc9c15049811b
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2023-08-31 20:50:08 +0200

    Update home.js

--- src/templates/home.js
+++ src/templates/home.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { Row, Col, Container, Card } from "react-bootstrap"
+import Layout from "../components/layout"
 
 //other
 import { StaticImage } from "gatsby-plugin-image"
@@ -21,75 +22,77 @@ const Home = () => {
     }
   `)
   return (
-    <Wrapper>
-      <section className="py-5">
-        <Container>
-          <Row>
-            <h1>{data.directus.cms_articles[0].title}</h1>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: data.directus.cms_articles[0].text,
-              }}
-            />
-          </Row>
-        </Container>
-      </section>
-      <section className="py-5 text-center news">
-        <Container>
-          <Row>
-            <Col sm={4} xs={12}>
-              <Card>
-                <StaticImage
-                  src="../images/progetti.png"
-                  formats={["AUTO", "WEBP"]}
-                  alt="progetti"
-                  className="card-img-top"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {" "}
-                    <a href="/">Sub Projects</a>
-                  </h5>
-                </div>
-              </Card>
-            </Col>
-            <Col sm={4} xs={12}>
-              <Card>
-                <StaticImage
-                  src="../images/news.png"
-                  formats={["AUTO", "WEBP"]}
-                  alt="news"
-                  className="card-img-top"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <a href="/">News</a>
-                  </h5>
-                </div>
-              </Card>
-            </Col>
-            <Col sm={4} xs={12}>
-              <Card>
-                <StaticImage
-                  src="../images/output.png"
-                  formats={["AUTO", "WEBP"]}
-                  alt="output"
-                  className="card-img-top"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <a href="/">Outputs</a>
-                  </h5>
-                </div>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </Wrapper>
+    <Layout>
+      <Wrapper>
+        <section className="py-5">
+          <Container>
+            <Row>
+              <h1>{data.directus.cms_articles[0].title}</h1>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: data.directus.cms_articles[0].text,
+                }}
+              />
+            </Row>
+          </Container>
+        </section>
+        <section className="py-5 text-center news">
+          <Container>
+            <Row>
+              <Col sm={4} xs={12}>
+                <Card>
+                  <StaticImage
+                    src="../images/progetti.png"
+                    formats={["AUTO", "WEBP"]}
+                    alt="progetti"
+                    className="card-img-top"
+                    height={300}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {" "}
+                      <a href="/">Sub Projects</a>
+                    </h5>
+                  </div>
+                </Card>
+              </Col>
+              <Col sm={4} xs={12}>
+                <Card>
+                  <StaticImage
+                    src="../images/news.png"
+                    formats={["AUTO", "WEBP"]}
+                    alt="news"
+                    className="card-img-top"
+                    height={300}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <a href="/">News</a>
+                    </h5>
+                  </div>
+                </Card>
+              </Col>
+              <Col sm={4} xs={12}>
+                <Card>
+                  <StaticImage
+                    src="../images/output.png"
+                    formats={["AUTO", "WEBP"]}
+                    alt="output"
+                    className="card-img-top"
+                    height={300}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <a href="/">Outputs</a>
+                    </h5>
+                  </div>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </Wrapper>
+    </Layout>
   )
 }
 
