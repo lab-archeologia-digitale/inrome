8f0360804084c40a2fa2cb385831fde6e4115f0a
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2023-09-03 12:10:39 +0200

    Update team.js

--- src/templates/team.js
+++ src/templates/team.js
@@ -3,6 +3,7 @@ import { Row, Col, Container, Card } from "react-bootstrap"
 import Layout from "../components/layout"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 const Team = (currentLang, currentPath) => {
   const data = useStaticQuery(graphql`
@@ -102,174 +103,203 @@ const Team = (currentLang, currentPath) => {
   `)
   return (
     <Layout>
-      <div>
-        <section className="py-5">
-          <Container>
-            <Row>
-              <h1>{data.directus.intro[0].title}</h1>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: data.directus.intro[0].text,
-                }}
-              />
-            </Row>
-          </Container>
-        </section>
+      <Wrapper>
+        <div>
+          <section className="py-5">
+            <Container>
+              <Row>
+                <h1>{data.directus.intro[0].title}</h1>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: data.directus.intro[0].text,
+                  }}
+                />
+              </Row>
+            </Container>
+          </section>
 
-        <section className="py-5 text-center">
-          <Container>
-            <Row>
-              <h2>Scuola Normale Superiore</h2>
-              {data.directus.sns.map((person, index) => {
-                return (
-                  <Col xs="6" lg="3" className="py-3" key={index}>
-                    <Card>
-                      <GatsbyImage
-                        image={
-                          person.image.imageFile.childImageSharp.gatsbyImageData
-                        }
-                        alt={person.title}
-                      />
-                      <Card.Body
-                        style={{
-                          color: "#ffffff",
-                          backgroundColor: person.color ?? "rgb(169, 158, 88)",
-                        }}
-                        className={person.color}
-                      >
-                        <Card.Title className="border-bottom">
-                          <Link
-                            to={`/en/${person.slug}`}
-                            style={{
-                              color: "#ffffff",
-                              textDecoration: "none",
-                            }}
-                          >
-                            {person.title}
-                          </Link>
-                        </Card.Title>
-                        <Card.Text
-                          className="text-center"
+          <section className="py-5 text-center">
+            <Container>
+              <Row>
+                <h2>Scuola Normale Superiore</h2>
+                {data.directus.sns.map((person, index) => {
+                  return (
+                    <Col xs="6" lg="3" className="py-3" key={index}>
+                      <Card>
+                        <GatsbyImage
+                          image={
+                            person.image.imageFile.childImageSharp
+                              .gatsbyImageData
+                          }
+                          alt={person.title}
+                        />
+                        <Card.Body
                           style={{
-                            fontSize: "0.7rem",
+                            color: "#ffffff",
+                            backgroundColor:
+                              person.color ?? "rgb(169, 158, 88)",
                           }}
-                          dangerouslySetInnerHTML={{
-                            __html: person.summary.replace(
-                              /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
-                              "$1<br />$2",
-                            ),
-                          }}
-                        ></Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                )
-              })}
-            </Row>
-            <Row>
-              <h2>Sapienza Università di Roma</h2>
-              {data.directus.sapienza.map((person, index) => {
-                return (
-                  <Col xs="6" lg="3" className="py-3" key={index}>
-                    <Card>
-                      <GatsbyImage
-                        image={
-                          person.image.imageFile.childImageSharp.gatsbyImageData
-                        }
-                        alt={person.title}
-                      />
-                      <Card.Body
-                        style={{
-                          color: "#ffffff",
-                          backgroundColor: person.color ?? "rgb(128, 36, 51)",
-                        }}
-                        className={person.color}
-                      >
-                        <Card.Title className="border-bottom">
-                          <Link
-                            to={`/en/${person.slug}`}
+                          className={person.color}
+                        >
+                          <Card.Title className="border-bottom">
+                            <Link
+                              className="card-link"
+                              to={`/en/${person.slug}`}
+                            >
+                              {person.title}
+                            </Link>
+                          </Card.Title>
+                          <Card.Text
+                            className="text-center"
                             style={{
-                              color: "#ffffff",
-                              textDecoration: "none",
+                              fontSize: "0.7rem",
                             }}
-                          >
-                            {person.title}
-                          </Link>
-                        </Card.Title>
-                        <Card.Text
-                          className="text-center"
+                            dangerouslySetInnerHTML={{
+                              __html: person.summary.replace(
+                                /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
+                                "$1<br />$2",
+                              ),
+                            }}
+                          ></Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  )
+                })}
+              </Row>
+              <Row>
+                <h2>Sapienza Università di Roma</h2>
+                {data.directus.sapienza.map((person, index) => {
+                  return (
+                    <Col xs="6" lg="3" className="py-3" key={index}>
+                      <Card>
+                        <GatsbyImage
+                          image={
+                            person.image.imageFile.childImageSharp
+                              .gatsbyImageData
+                          }
+                          alt={person.title}
+                        />
+                        <Card.Body
                           style={{
-                            fontSize: "0.7rem",
+                            color: "#ffffff",
+                            backgroundColor: person.color ?? "rgb(128, 36, 51)",
                           }}
-                          dangerouslySetInnerHTML={{
-                            __html: person.summary.replace(
-                              /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
-                              "$1<br />$2",
-                            ),
-                          }}
-                        ></Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                )
-              })}
-            </Row>
-            <Row>
-              <h2>AIT Austrian Institute of Technology</h2>
-              {data.directus.ait.map((person, index) => {
-                return (
-                  <Col xs="6" lg="3" className="py-3" key={index}>
-                    <Card>
-                      <GatsbyImage
-                        image={
-                          person.image.imageFile.childImageSharp.gatsbyImageData
-                        }
-                        alt={person.title}
-                      />
-                      <Card.Body
-                        style={{
-                          color: "#ffffff",
-                          backgroundColor: person.color ?? "rgb(187, 187, 187)",
-                        }}
-                        className={person.color}
-                      >
-                        <Card.Title className="border-bottom">
-                          <Link
-                            to={`/en/${person.slug}`}
+                          className={person.color}
+                        >
+                          <Card.Title className="border-bottom">
+                            <Link
+                              className="card-link"
+                              to={`/en/${person.slug}`}
+                            >
+                              {person.title}
+                            </Link>
+                          </Card.Title>
+                          <Card.Text
+                            className="text-center"
                             style={{
-                              color: "#ffffff",
-                              textDecoration: "none",
+                              fontSize: "0.7rem",
                             }}
-                          >
-                            {person.title}
-                          </Link>
-                        </Card.Title>
-                        <Card.Text
-                          className="text-center"
+                            dangerouslySetInnerHTML={{
+                              __html: person.summary.replace(
+                                /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
+                                "$1<br />$2",
+                              ),
+                            }}
+                          ></Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  )
+                })}
+              </Row>
+              <Row>
+                <h2>AIT Austrian Institute of Technology</h2>
+                {data.directus.ait.map((person, index) => {
+                  return (
+                    <Col xs="6" lg="3" className="py-3" key={index}>
+                      <Card>
+                        <GatsbyImage
+                          image={
+                            person.image.imageFile.childImageSharp
+                              .gatsbyImageData
+                          }
+                          alt={person.title}
+                        />
+                        <Card.Body
                           style={{
-                            fontSize: "0.7rem",
+                            color: "#ffffff",
+                            backgroundColor:
+                              person.color ?? "rgb(187, 187, 187)",
                           }}
-                          dangerouslySetInnerHTML={{
-                            __html: person.summary.replace(
-                              /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
-                              "$1<br />$2",
-                            ),
-                          }}
-                        ></Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                )
-              })}
-            </Row>
-            <Row>
-              <h2>Advisory Board </h2>
-            </Row>
-          </Container>
-        </section>
-      </div>
+                          className={person.color}
+                        >
+                          <Card.Title className="border-bottom">
+                            <Link
+                              className="card-link"
+                              to={`/en/${person.slug}`}
+                            >
+                              {person.title}
+                            </Link>
+                          </Card.Title>
+                          <Card.Text
+                            className="text-center"
+                            style={{
+                              fontSize: "0.7rem",
+                            }}
+                            dangerouslySetInnerHTML={{
+                              __html: person.summary.replace(
+                                /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
+                                "$1<br />$2",
+                              ),
+                            }}
+                          ></Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  )
+                })}
+              </Row>
+              <Row>
+                <h2>Advisory Board </h2>
+              </Row>
+            </Container>
+          </section>
+        </div>
+      </Wrapper>
     </Layout>
   )
 }
+//style
+const Wrapper = styled.section`
+  .card {
+    border-radius: 1rem;
+
+    &:hover img {
+      opacity: 0.7 !important;
+      transition: all 1s ease;
+    }
+
+    .card-title a {
+      color: #fff;
+      text-decoration: none;
+
+      &:hover {
+        color: #000;
+        text-decoration: none;
+      }
+    }
+
+    .card-link a {
+      color: #ffffff;
+      text-decoration: none
+      
+      &:hover {
+        color: #000;
+        text-decoration: none;
+      }
+    }
+  }
+`
 
 export default Team
